f0dda2480082db7dd2783c195e808f33cfbf924f
eduardo01322 <eduardo.errea32@gmail.com>
2023-09-14 14:58:58 -0300

    serviços

---
+++ app/Http/Controllers/ServicoController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServicoFormRequest;
+use App\Models\servico;
+use Illuminate\Http\Request;
+
+class ServicoController extends Controller
+
+{
+    public function Servicos(ServicoFormRequest $request){
+        $servico= servico::create([
+            'nome'=> $request ->nome,
+            'descricao'=> $request ->descricao,
+            'duracao'=> $request ->duracao,
+            'preco'=> $request ->preco,
+        ]);
+
+        return response()->json([
+            "success" => true,
+            "message" => "usuario cadastrado com sucesso",
+            "data" => $servico
+        ], 200);
+}
+
+public function pesquisarPorNome(Request $request){
+    $servico =  servico::where('nome', 'like', '%'. $request->nome . '%')->get();
+    if(count($servico) > 0){
+    return response()->json([
+        'status'=> true,
+        'data '=> $servico
+    ]);
+}
+
+return response()->json([
+    'status' => false,
+    'message' => 'não há resultados para pesquisa.'
+]);
+}
+
+public function pesquisarPorDescricao(Request $request){
+    $servico =  servico::where('descricao', 'like', '%'. $request->descricao . '%')->get();
+    if(count($servico) > 0){
+    return response()->json([
+        'status'=> true,
+        'data '=> $servico
+    ]);
+}
+
+return response()->json([
+    'status' => false,
+    'message' => 'não há resultados para pesquisa.'
+]);
+}
+
+public function pesquisarPorId($id){
+    $servico = servico::find($id);
+    if($servico == null){
+        return response()->json([
+            'status'=> false,
+            'message' => "usuario não encontrado"
+        ]);     
+    }
+    return response()->json([
+        'status'=> true,
+        'data '=> $servico
+    ]);
+}
+
+public function excluir($id){
+    $servico = servico::find($id);
+    if(!isset($servico)){
+        return response()->json([
+            "status" => false,
+            "message" => "usuario não encontrado"
+        ]);
+    }
+    $servico->delete();
+
+    return response()->json([
+        'status' => false,
+        'message' => 'usuario excluido com sucesso'
+    ]);
+}
+
+public function update(Request $request){
+    $servico = servico::find($request->id);
+
+    if(!isset($servico)){
+        return response()->json([
+            "status" => false,
+            "message" => "usuario não encontrado"
+        ]);
+    }
+
+    if(isset($request->nome)){
+        $servico->nome = $request->nome;
+    }
+    if(isset($request->descrisao)){
+        $servico->descrisao = $request->descrisao;
+    }
+    if(isset($request->duracao)){
+        $servico->duracao = $request->duracao;
+    }
+    if(isset($request->preco)){
+        $servico->preco = $request->preco;
+    }
+
+    $servico->update();
+
+    return response()->json([
+        "status" => false,
+        "message" => "usuario atualizado"
+    ]);
+
+}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
+use App\Http\Controllers\ServicoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+route::post('servicos', [ServicoController::class, 'servicos']);
+route::post('descricao', [ServicoController::class, 'pesquisarPorDescricao']);
+route::post('nome', [ServicoController::class, 'pesquisarPorNome']);
+route::get('find/{id}', [ServicoController::class, 'pesquisarPorId']);
+route::delete('delete/{id}', [ServicoController::class, 'excluir']);
+route::put('update', [ServicoController::class, 'update']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
